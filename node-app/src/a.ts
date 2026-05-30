39958a43b6542d80b16956e80875bb0a06f975d8
ashu <ashutoshvishwakarma222@gmail.com>
2026-05-30 14:15:05 +0530

    useing interfaces

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -1,5 +1,29 @@
-function Sum(num1: number, num2: number): number{
-    return num1 + num2;
+interface User {
+    firstName: string,
+    lastName: string,
+    age: number
+};
+
+function isLegal(user: User){
+    if(user.age > 18){
+        console.log("User is legal");
+    } else {
+        console.log("User is illegal");
+    }
 }
 
-console.log(Sum(1,2));
+function greet(user: User){
+    console.log("Hello "+user.firstName);
+}
+
+isLegal({
+    firstName: "Ashutosh",
+    lastName: "Vishwakarma",
+    age: 23
+})
+
+greet({
+    firstName: "Ashutosh",
+    lastName: "Vishwakarma",
+    age: 23
+})
